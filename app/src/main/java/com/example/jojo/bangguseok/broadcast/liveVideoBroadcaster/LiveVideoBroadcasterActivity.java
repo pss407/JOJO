4d67d2287930c31039096ff2e46d511db1e37f13
wlshddlek <42800352+wlshddlek@users.noreply.github.com>
2019-11-04 19:19:16 +0900

    super

--- app/src/main/java/com/example/jojo/bangguseok/broadcast/liveVideoBroadcaster/LiveVideoBroadcasterActivity.java
+++ app/src/main/java/com/example/jojo/bangguseok/broadcast/liveVideoBroadcaster/LiveVideoBroadcasterActivity.java
@@ -87,6 +87,8 @@ import java.util.TimerTask;
 import static com.example.jojo.bangguseok.broadcast.BroadcastActivity.RTMP_BASE_URL;
 
 
+
+
 /////////////////
 
 import android.content.Intent;
@@ -146,8 +148,42 @@ import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
 
+//채팅 추가
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jojo.bangguseok.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class LiveVideoBroadcasterActivity extends AppCompatActivity implements View.OnClickListener, ExoPlayer.EventListener,
         PlaybackControlView.VisibilityListener{
+
+    //채팅 변수
+
+    ArrayList<com.example.jojo.bangguseok.chatting.ChatVO> list = new ArrayList<>();
+    ListView lv;
+    Button btn;
+    EditText edt;
+    int imageID = R.drawable.profile;
+
+    String id = "";
+    String message = "message";
+    int chatId = 0;
+    //
 
     public static final String PREFER_EXTENSION_DECODERS = "prefer_extension_decoders";
 
@@ -198,6 +234,8 @@ public class LiveVideoBroadcasterActivity extends AppCompatActivity implements V
 
     /** Defines callbacks for service binding, passed to bindService() */
     private ServiceConnection mConnection = new ServiceConnection() {
+
+
 
         @Override
         public void onServiceConnected(ComponentName className,
@@ -361,6 +399,84 @@ public class LiveVideoBroadcasterActivity extends AppCompatActivity implements V
 
         simpleExoPlayerView.bringToFront();
 
+
+
+        //채팅 추가
+
+
+        lv = findViewById(R.id.listView);
+        edt = findViewById(R.id.editText);
+        btn = findViewById(R.id.bnt_send);
+
+// Write a message to the database
+        chatId = getIntent().getIntExtra("chatId",0);
+        message = message.concat(Integer.toString(chatId));
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference(message);
+
+//로그인한 아이디
+        id = getIntent().getStringExtra("id");
+
+//list.add(new ChatVO(R.drawable.profile3, "찡찡이", "안녕", "오후 4:42"));
+
+        final com.example.jojo.bangguseok.chatting.ChatAdapter adapter = new com.example.jojo.bangguseok.chatting.ChatAdapter(getApplicationContext(), R.layout.talklist, list, id);
+        ((ListView) findViewById(R.id.listView)).setAdapter(adapter);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (edt.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "내용을 입력하세요.", Toast.LENGTH_LONG).show();
+                } else {
+                    Date today = new Date();
+                    SimpleDateFormat timeNow = new SimpleDateFormat("a K:mm");
+
+                    StringBuffer sb = new StringBuffer(edt.getText().toString());
+                    if (sb.length() >= 15) {
+                        for (int i = 1; i <= sb.length() / 15; i++) {
+                            sb.insert(15 * i, "\n");
+                        }
+                    }
+
+//list.add(new ChatVO(R.drawable.profile1, id, sb.toString(), timeNow.format(today)));
+//adapter.notifyDataSetChanged();
+
+                    myRef.push().setValue(new com.example.jojo.bangguseok.chatting.ChatVO(R.drawable.profile, id, sb.toString(), timeNow.format(today)));
+                    edt.setText("");
+                }
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                com.example.jojo.bangguseok.chatting.ChatVO value = dataSnapshot.getValue(com.example.jojo.bangguseok.chatting.ChatVO.class); // 괄호 안 : 꺼낼 자료 형태
+                list.add(value);
+                adapter.notifyDataSetChanged();
+                lv.setSelection(adapter.getCount() - 1);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
 
