ae7ac6c32784d880ee043e63c914f2c2342da333
wlshddlek <42800352+wlshddlek@users.noreply.github.com>
2019-11-17 15:32:29 +0900

    super

--- app/src/main/java/com/example/jojo/bangguseok/broadcast/liveVideoBroadcaster/LiveVideoBroadcasterActivity.java
+++ app/src/main/java/com/example/jojo/bangguseok/broadcast/liveVideoBroadcaster/LiveVideoBroadcasterActivity.java
@@ -222,7 +222,7 @@ public class LiveVideoBroadcasterActivity extends AppCompatActivity implements V
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+  //
         // Hide title
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
