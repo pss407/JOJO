5a7e8e43bea443e1ff8b2811576758a95c352b05
wlshddlek <42800352+wlshddlek@users.noreply.github.com>
2019-12-01 15:50:17 +0900

    super

--- app/src/main/java/com/example/jojo/bangguseok/broadcast/marking.java
+++ app/src/main/java/com/example/jojo/bangguseok/broadcast/marking.java
@@ -1,11 +1,17 @@
 package com.example.jojo.bangguseok.broadcast;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.jojo.bangguseok.R;
+
+import java.io.FileOutputStream;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
@@ -20,6 +26,11 @@ public class marking extends AppCompatActivity {
 
     TextView pitchText;
     TextView noteText;
+    TextView textView11;
+    String mus="";
+    float pitchInHz_tmp;
+    int count=0;
+    AudioDispatcher dispatcher;
 
 
 
@@ -30,10 +41,9 @@ public class marking extends AppCompatActivity {
 
         noteText = (TextView) findViewById(R.id.noteText);
         pitchText = (TextView) findViewById(R.id.pitchText);
+        textView11 = (TextView) findViewById(R.id.textView11);
 
-
-
-        AudioDispatcher dispatcher =
+        dispatcher =
                 AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
 
         PitchDetectionHandler pdh = new PitchDetectionHandler() {
@@ -43,7 +53,10 @@ public class marking extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        processPitch(pitchInHz);
+                        pitchInHz_tmp=pitchInHz;
+                        func();
+                        //processPitch(pitchInHz);
+
                     }
                 });
             }
@@ -56,39 +69,165 @@ public class marking extends AppCompatActivity {
 
 
 
+
+
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        releaseDispatcher();
+    }
+
+    public void releaseDispatcher()
+    {
+        if(dispatcher != null)
+        {
+            if(!dispatcher.isStopped())
+                dispatcher.stop();
+            dispatcher = null;
+        }
+    }
+
+    public void func()
+    {
+
+        Handler delayHandler6 = new Handler();
+        delayHandler6.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                processPitch(pitchInHz_tmp);
+                if(count<=20) {
+                    func();
+                }
+                count++;
+            }
+        }, 2000);
+
+
+    }
+
+    public void internalStorageSaveFile()
+    {
+        String filename = "myfile";
+        String string = mus;
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+
+            Toast.makeText(this, "this is internal storage save success.", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            Toast.makeText(this, "this is internal storage save fail.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void  save2(View view) {
+
+        internalStorageSaveFile();
+    }
+
+    public void button13(View view) {
+
+
+
+    }
+
+    public void see_mus(View view) {
+
+        textView11.setText(mus);
+    }
+
+    public void stringreset(View view) {
+
+      mus="";
     }
 
     public void processPitch(float pitchInHz) {
 
         pitchText.setText("" + pitchInHz);
 
-        if(pitchInHz >= 110 && pitchInHz < 123.47) {
+        if(pitchInHz < 137) {
             //A
-            noteText.setText("A");
+            noteText.setText("낮은 도");
+            mus+="a";
+
         }
-        else if(pitchInHz >= 123.47 && pitchInHz < 130.81) {
+        else if(pitchInHz >= 144&& pitchInHz < 153) {
             //B
-            noteText.setText("B");
+            noteText.setText("낮은 레");
+            mus+="b";
         }
-        else if(pitchInHz >= 130.81 && pitchInHz < 146.83) {
+        else if(pitchInHz >= 160 && pitchInHz < 170) {
             //C
-            noteText.setText("C");
+            noteText.setText("낮은 미");
+            mus+="c";
         }
-        else if(pitchInHz >= 146.83 && pitchInHz < 164.81) {
+        else if(pitchInHz >= 171 && pitchInHz < 180) {
             //D
-            noteText.setText("D");
+            noteText.setText("낮은 파");
+            mus+="d";
         }
-        else if(pitchInHz >= 164.81 && pitchInHz <= 174.61) {
+        else if(pitchInHz >= 190 && pitchInHz <=203) {
             //E
-            noteText.setText("E");
+            noteText.setText("낮은 솔");
+            mus+="e";
         }
-        else if(pitchInHz >= 174.61 && pitchInHz < 185) {
+        else if(pitchInHz >= 215 && pitchInHz < 230) {
             //F
-            noteText.setText("F");
+            noteText.setText("낮은 라");
+            mus+="f";
         }
-        else if(pitchInHz >= 185 && pitchInHz < 196) {
+        else if(pitchInHz >= 243 && pitchInHz < 253) {
             //G
-            noteText.setText("G");
+            noteText.setText("낮은 시");
+            mus+="g";
         }
+        else if(pitchInHz >= 259 && pitchInHz < 168) {
+            //G
+            noteText.setText("도");
+            mus+="A";
+        }
+        else if(pitchInHz >= 290 && pitchInHz <303) {
+            //G
+            noteText.setText("레");
+            mus+="B";
+        }
+        else if(pitchInHz >= 327 && pitchInHz < 339) {
+            //G
+            noteText.setText("미");
+            mus+="C";
+        }
+        else if(pitchInHz >= 347 && pitchInHz < 359) {
+            //G
+            noteText.setText("파");
+            mus+="D";
+        }
+        else if(pitchInHz >= 386 && pitchInHz < 399) {
+            //G
+            noteText.setText("솔");
+            mus+="E";
+        }
+        else if(pitchInHz >= 435 && pitchInHz < 448) {
+            //G
+            noteText.setText("라");
+            mus+="F";
+        }
+        else if(pitchInHz >= 490 ) {
+            //G
+            noteText.setText("시");
+            mus+="G";
+        }
+
     }
 }
